d08dacdcf8ddd7aabc33966bc49f9e0c16f2b1a7
Cobus Bernard <cobusb@amazon.com>
2019-11-06 09:43:57 +0200

    Demo of multi-account terraform setup from last night

---
+++ infra-envs/terraform-state.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket  = "my-terraform-statefile-bucket-name-here"
+    key     = "statefiles/environments"
+    region  = "eu-west-1"
+    profile = "meetup-main"
+  }
+}
